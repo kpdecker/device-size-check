b2e186724773aa47fc6b0c1d090ce6c457ee1d28
kpdecker <kpdecker@gmail.com>
2012-04-04 10:30:41 -0500

    Test Collector

---
+++ js/test.js
@@ -0,0 +1,30 @@
+function testComplete() {
+  var docEl = document.documentElement;
+
+  window.opener.testOutput.tests.push({
+    name: window.title,
+    orrientation: window.orrientation,
+    screen: {
+      width: window.screen.width,
+      height: window.screen.height,
+      availWidth: window.screen.availWidth,
+      availHeight: window.screen.availHeight
+    },
+    window: {
+      innerWidth: window.innerWidth,
+      innerHeight: window.innerHeight,
+      outerWidth: window.outerWidth,
+      outerHeight: window.outerHeight
+    },
+    documentElement: {
+      clientWidth: docEl.clientWidth,
+      clientHeight: docEl.clientHeight,
+      offsetWidth: docEl.offsetWidth,
+      offsetHeight: docEl.offsetHeight,
+      scrollWidth: docEl.scrollWidth,
+      scrollHeight: docEl.scrollHeight
+    }
+  });
+  window.opener.runTest();
+  window.close();
+}
