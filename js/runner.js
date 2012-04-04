0aac917be70ea02376166ca9525b90b9a3c94e8f
kpdecker <kpdecker@gmail.com>
2012-04-04 14:07:59 -0500

    Add user-level error handling to runTest

--- js/runner.js
+++ js/runner.js
@@ -15,37 +15,42 @@ var testOutput = {};
 
 function runTest() {
   if (!testList.length) {
-    clearTimeout(timeout);
+    try {
+      clearTimeout(timeout);
 
-    // Flatten the structure for browserscope
-    var ret = {
-      orientation: window.orientation,
-      devicePixelRatio: window.devicePixelRatio
-    };
-    _.each(testOutput, function(test, testName) {
-      _.each(test, function(metric, metricName) {
-        if (metricName !== 'name') {
-          _.each(metric, function(value, valueName) {
-            ret[[testName, metricName, valueName].join('_')] = value;
-          });
-        }
+      // Flatten the structure for browserscope
+      var ret = {
+        orientation: window.orientation,
+        devicePixelRatio: window.devicePixelRatio
+      };
+      _.each(testOutput, function(test, testName) {
+        _.each(test, function(metric, metricName) {
+          if (metricName !== 'name') {
+            _.each(metric, function(value, valueName) {
+              ret[[testName, metricName, valueName].join('_')] = value;
+            });
+          }
+        });
       });
-    });
-    window._bTestResults = ret;
+      window._bTestResults = ret;
 
-    // Beacon the results to Browserscope.
-    var testKey = 'agt1YS1wcm9maWxlcnINCxIEVGVzdBjrw4wQDA';
-    var newScript = document.createElement('script'),
-        firstScript = document.getElementsByTagName('script')[0];
-    newScript.src = 'http://www.browserscope.org/user/beacon/' + testKey;
-    firstScript.parentNode.insertBefore(newScript, firstScript);
+      // Beacon the results to Browserscope.
+      var testKey = 'agt1YS1wcm9maWxlcnINCxIEVGVzdBjrw4wQDA';
+      var newScript = document.createElement('script'),
+          firstScript = document.getElementsByTagName('script')[0];
+      newScript.src = 'http://www.browserscope.org/user/beacon/' + testKey;
+      firstScript.parentNode.insertBefore(newScript, firstScript);
 
-    // Output for the user
-    $('#content').text(JSON.stringify({
-        userAgent: navigator.userAgent,
-        test: testOutput
-      }, undefined, 2));
-    testWin.close();
+      // Output for the user
+      $('#content').text(JSON.stringify({
+          userAgent: navigator.userAgent,
+          test: testOutput
+        }, undefined, 2));
+      testWin.close();
+    } catch (err) {
+      $(document.body).append($('<div>').text(err.stack || err));
+      testWin.close();
+    }
   } else {
     var test = testList.shift(),
         root = location.href.replace(/\/([^\/]*?\.[^\/]*?)?$/, '');
