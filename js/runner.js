0fcfcef195108c82b5144fa19d4ebcedf916f39e
kpdecker <kpdecker@gmail.com>
2012-04-04 11:43:27 -0500

    Explicitly define test root

--- js/runner.js
+++ js/runner.js
@@ -13,7 +13,8 @@ function runTest() {
   if (!testList.length) {
     $('#content').text(JSON.stringify(testOutput, undefined, 2));
   } else {
-    var test = testList.shift();
-    window.open(test);
+    var test = testList.shift(),
+        root = location.href.replace(/\/([^\/]*?\.[^\/]*?)?$/, '');
+    window.open(root + '/' + test, 'device-size-check-test');
   }
 }
